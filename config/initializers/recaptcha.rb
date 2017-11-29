e1b2883335ab063c400431da0bd0a40e41460aee
gabrieelgomez <gagg1707@gmail.com>
2017-11-29 16:03:59 -0400

    key de recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,5 @@
 Recaptcha.configure do |config|
   # Localhost keys
-  config.site_key = Rails.application.secrets.site_key
-  config.secret_key = Rails.application.secrets.secret_key
+  config.site_key = "6LeL8DoUAAAAAA53YMg1D6GwfW8CsuUD_z31D4js"
+  config.secret_key = "6LeL8DoUAAAAAMuPNzMlNuQlh2KJ-5TZKMcWsLBo"
 end
-  
